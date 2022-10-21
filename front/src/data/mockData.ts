55212d04e987b24eca0349b0fe27783872d08a3a
Pavel Murygin <pavel.murygin@gmail.com>
2022-10-21 14:11:05 +0400

    less data

--- front/src/data/mockData.ts
+++ front/src/data/mockData.ts
@@ -11,7 +11,7 @@ const mockdata = [
         services: [
             '6c283abf-735d-4c5b-8ee3-ab6f97e19fe9',
             '7416f7ae-0a0d-42ac-9223-6ce36a75aa8e',
-            '62a0064c-e35a-4d31-82bc-b4600daba34c',
+            //'62a0064c-e35a-4d31-82bc-b4600daba34c',
             'be919e08-2b06-475a-9b2f-ad8773c90f3a',
         ],
     },
@@ -20,7 +20,7 @@ const mockdata = [
         services: [
             '34e85d52-b0ad-454f-a48d-0eb79507cdf7',
             '677b2962-9160-422a-869e-bd30d02b9ff4',
-            '875e2067-5a58-4b57-9fbe-73b17869e4d7',
+            //'875e2067-5a58-4b57-9fbe-73b17869e4d7',
             '91bf0b97-301a-4dfe-bcec-72fdb225374b',
         ],
     },
@@ -41,7 +41,7 @@ const mockdata = [
         services: [
             '395c8723-7f80-48b7-8960-96056e143f7c',
             '248d27e9-aded-4c74-902e-ecc67bac0973',
-            '95f19b0c-cdaa-4185-a96f-4fe972b3f407',
+            //'95f19b0c-cdaa-4185-a96f-4fe972b3f407',
             '773abc1d-c7f2-49cd-84da-25c911baec4e',
         ],
     },
@@ -50,7 +50,7 @@ const mockdata = [
         services: [
             '7ae8822b-525f-402b-8a2f-593042f6a1ea',
             '4073524b-61e5-4ac5-ac23-551d32c765d5',
-            '6b151bff-b4e7-4d14-9796-199be5918b62',
+            //'6b151bff-b4e7-4d14-9796-199be5918b62',
             '345ad069-f872-4765-a2c9-733507d21284',
         ],
     },
